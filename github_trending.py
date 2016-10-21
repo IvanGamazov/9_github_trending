1745fc4854788fa5083da139af5dbb2b11e7f5a0
IvanGamazov <ivan.gamazov@gmail.com>
2016-10-21 14:38:38 +0300

    Функции стали чистыми, запрос к API выполнен по-другому.
    Дописан Readme

--- github_trending.py
+++ github_trending.py
@@ -2,23 +2,27 @@ import requests
 import datetime
 
 
-def get_time_period():
+GITHUB_API_URL = 'https://api.github.com/search/repositories?q=created:>'
+
+
+def input_time_period():
     return int(input('За какой промежуток времени искать репозитории? -->'))
 
 
-def get_quantity_for_search():
+def input_quantity_for_search():
     return int(input('Сколько репозиториев подбирать? -->'))
 
 
-def get_start_search_date():
-    search_date = datetime.date.today() - datetime.timedelta(days=get_time_period())
+def get_start_search_date(days):
+    search_date = datetime.date.today() - datetime.timedelta(days=days)
     return search_date
 
 
-def get_trending_repositories():
-    response = requests.request('GET', 'https://api.github.com/search/repositories?q=created:>'
-                                + str(get_start_search_date())+'&sort=stars&order=desc')
-    repositories = response.json()['items'][:get_quantity_for_search()]
+def get_trending_repositories(date, repos_count):
+    str_date = str(date)
+    payload = {'q': 'created:>'+str_date, 'sort': 'stars', 'order': 'desc'}
+    response = requests.get(GITHUB_API_URL, params=payload)
+    repositories = response.json()['items'][:repos_count]
     return repositories
 
 
@@ -34,11 +38,15 @@ def get_top_repos(repositories):
 
 
 if __name__ == '__main__':
-    repositories_list = get_trending_repositories()
+    length_in_days = input_time_period()
+    start_date = get_start_search_date(length_in_days)
+    amount_of_repos = input_quantity_for_search()
+    repositories_list = get_trending_repositories(start_date, amount_of_repos)
     top_repositories = get_top_repos(repositories_list)
     sorted_repos = sorted(top_repositories, key=lambda rep: rep['issues'])
     print('Популярные репозитории:')
     for repo in sorted_repos:
-        print('Название:', repo['name'], ' Ссылка:', repo['url'], ' Проблем:', repo['issues'], ' Звезд:', repo['stars'])
+        print('Название:', repo['name'], ' Ссылка:', repo['url'],
+              ' Проблем:', repo['issues'], ' Звезд:', repo['stars'])
 
 
